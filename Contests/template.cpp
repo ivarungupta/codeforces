789c5953bfa6d571afc09199499e39d09388a6f0
contactvarungupta <v.g7404@gmail.com>
2024-07-20 01:48:20 +0530

    4th commit

---
+++ Contests/template.cpp
@@ -0,0 +1,29 @@
+/*
+__     _______
+\ \   / / ____|
+ \ \ / / |  __    /\_/\
+  \ V /| | |_ |  ( o.o )
+   \ / | |__| |   > ^ <
+    V   \_____|
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vec vector
+#define pb push_back
+#define pq priority_queue
+#define mp make_pair
+#define MOD 1000000007
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    return 0;
+}

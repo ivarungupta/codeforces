d688b56668e06deb4d461a0d6256449337f9d97d
contactvarungupta <v.g7404@gmail.com>
2024-08-16 01:36:35 +0530

    x

---
+++ Questions/CF46A.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    static bool lemonadeChange(vector<int>& bills) {
+        int cnt5=0, cnt10=0;
+        for(int b: bills){
+            switch(b){
+                case 5: 
+                    cnt5++; 
+                    break;
+                case 10: 
+                    if (cnt5>0) cnt5--, cnt10++;
+                    else return 0;
+                    break;
+                case 20:
+                    if (cnt10>0 && cnt5>0) cnt10--, cnt5--;
+                    else if (cnt5>2) cnt5-=3;
+                    else return 0;
+                    break;
+            }
+        }
+        return 1;
+    }
+};
+
